a13d5aff6c465025447ca42577a8434d81e4c3b3
anjali-bora <boraanjali99@gmail.com>
2020-05-30 17:47:48 +0530

    adding part time condition to the program

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,11 +3,15 @@ class EmployeeWage
   public static void main(String[] args)
   {
     int Is_Full_Time=1;
+    int Is_Part_Time=2;
+
     int Emp_Rate_Per_Hour=20;
     int emphr=0;
     int empwage=0;
-    double empCheck = Math.floor(Math.random() * 10) % 2;
-    if (empCheck == Is_Full_Time)
+    double empCheck = Math.floor(Math.random() * 10) % 3;
+    if (empCheck == Is_Part_Time)
+	emphr=4;
+    else if (empCheck == Is_Full_Time)
         emphr=8;
     else
         emphr=0;
