38a98aa6b7be82d8d943dce62f11b0fad775d711
anjali-bora <boraanjali99@gmail.com>
2020-06-05 18:35:03 +0530

    it will show how much time will head and tail occured

---
+++ CoinFlipSimulation.sh
@@ -0,0 +1,15 @@
+  GNU nano 4.9.2                                                                                               coinflipsimulation.sh
+#!/bin/bash -x
+headcount=0
+tailcount=0
+for (( i=0; i<10; i++ ))
+do
+toss=$(($(($RANDOM%10))%2))
+if [ $toss -eq 1 ];then
+   ((headcount++))
+else
+  ((tailcount++))
+fi
+done
+echo "head occured : " $headcount
+echo "tails occured : " $tailcount
